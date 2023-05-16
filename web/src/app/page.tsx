f94ecddef2e7a7a221e2e50f379510fbf2e6f6e7
Alexandre Marques Bulcao <58255516+ambulcao@users.noreply.github.com>
2023-05-16 13:22:36 +0000

    Setup landpage web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-   <h1>Time Line</h1>
+    <div className='h-screen bg-zinc-950 text-zinc-50 p-6'>
+      <h1 className='text-4xl font-bold'>Sua cápsula do tempo</h1>
+    </div>
   )
 }
